49315dad458c3b02342bedaa5e76fefa97d1e71c
Frank Desktop <fguevara89@gmail.com>
2025-10-13 22:20:30 -0700

    Finished first assignment

--- Datatypes.py
+++ Datatypes.py
@@ -1,3 +1,29 @@
+import sys
+
 my_cool_variable = 42
 print(my_cool_variable)
 
+numbers = [1, 2, 3, 4, 5]
+
+intergers = (1, 2, 3, 4, 5)
+
+floats = {1.1, 2.2, 3.3, 4.4, 5.5}
+
+
+my_first_int = 89
+
+my_second_int = 43
+
+my_large_int = 10**101
+
+
+print(sys.getsizeof(my_first_int))
+print(sys.getsizeof(my_large_int))
+
+
+my_first_float = 3.192
+my_second_float = 2.0
+
+print(sys.getsizeof(my_first_float))
+
+print()

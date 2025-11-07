6f0d388d583d0b8409b9acc3ffe758cb95cfd794
Frank Desktop <fguevara89@gmail.com>
2025-11-06 21:47:07 -0800

    Making strings

---
+++ Testing.py
@@ -0,0 +1,23 @@
+my_first_string = "Hello, Worlds!"
+
+my_second_string = "41"
+
+
+# print(my_first_string)
+
+# print(my_first_string + " " + my_second_string)
+ 
+
+# print(f"My first string was {my_first_string}")
+
+
+my_multiline_string = """this is a multiline string
+next line
+Third line
+"""
+
+my_first_int = 42
+
+# print(f"My multiline string is: {my_multiline_string}")
+
+print(type(my_first_string))

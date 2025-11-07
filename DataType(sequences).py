058f9e1a8008a075989479c9dfcfd43da114bd8a
Frank Desktop <fguevara89@gmail.com>
2025-11-06 22:28:21 -0800

    Made new data type files

---
+++ DataType(sequences).py
@@ -0,0 +1,28 @@
+my_list_var = [
+    1,
+    2,
+    354,
+    4,
+    13,
+    56,
+    89,
+    True,
+    "Hello, World!",
+
+]
+
+# print(my_list_var[-1])
+
+# my_list_var[-1] = "Bannana"
+# print(my_list_var)
+
+
+my_first_tuple = (
+    1,
+    2,
+    100,
+    "apple",
+    True
+)
+
+print(my_first_tuple[2:5])
